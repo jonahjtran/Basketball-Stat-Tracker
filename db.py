3a38f08559c203226558ad5c467c0265052b1149
Jonah Tran <jonahtran@Jonahs-MacBook-Pro-76.local>
2025-07-13 23:02:29 -0500

    started database

---
+++ db.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+class Event:
+    def __init__(self):
+        self.
