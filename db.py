e0cd4da8d2f907d8d0e098f15c9291ca83556e87
Jonah Tran <jonahtran@Jonahs-MacBook-Pro-76.local>
2025-07-13 23:03:02 -0500

    started database

--- db.py
+++ db.py
@@ -8,6 +8,3 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-class Event:
-    def __init__(self):
-        self.
