0cd27ffcbedb8156b95dcd309ff9e70692b6eee7
Jonah Tran <jonahjtran@gmail.com>
2025-07-15 14:53:35 -0500

    starting stat_analysis

--- analytics.py
+++ analytics.py
@@ -7,6 +7,10 @@ def calculate_fg_percentage(average, attempts, made_shot):
     if made_shot:
         new_makes += 1
     new_attempts = attempts + 1
-    new_average = new_makes / new_attempts
+    new_average = float(new_makes)/float(new_attempts)
+    return round(new_average * 100)
 
-def calculate_zone_
+# parameters: x_coord and y_coord of shot
+# returns ShotZone of shot
+def define_shot_zone(x, y):
+    
