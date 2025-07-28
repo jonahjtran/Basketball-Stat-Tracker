a70aee2cb2564d0272db9f23492f3e8453e638f8
Jonah Tran <jonahjtran@gmail.com>
2025-07-27 22:32:46 -0500

    deleted player_heatmap

--- stats_tracker/games/views.py
+++ stats_tracker/games/views.py
@@ -10,16 +10,3 @@ from .utility.supabase_utility import upload_heatmap_to_supabase
 
 
 # Create your views here.
-
-def player_heatmap(request, game_id, player_id):
-    try:
-        game   = Game.objects.get(supabase_id=game_id)
-        events = game.events.filter(player__supabase_id=player_id)
-    except Game.DoesNotExist:
-        raise Http404("Game not found")
-
-    hm = Heatmap(events)
-    img_bytes = hm.save_as_image()  # modify to return BytesIO
-
-    return FileResponse(img_bytes, content_type='image/png')
-
